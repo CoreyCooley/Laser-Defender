457a3a74201c1eef6711109f7c0878de1605794c
Corey Cooley <corey.a.cooley@gmail.com>
2017-12-30 23:17:21 -0500

    Moving Enemy Formation
    Move enemy formation left to right and down.

--- Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
     public GameObject enemyPrefab;
+    public float speed = 5f;
+    public float flySpeed = 10f;
+    public float width = 10f;
+    public float height = 5f;
+    public float xPadding = 0.5f;
+    public float yPadding = 0.5f;
 
-	// Use this for initialization
-	void Start () {
+    // Private Variables
+    private bool isMovingRight = true;
+    private float xMin = -5.0f;
+    private float xMax = 5.0f;
+    private float yMin = -4.5f;
+    private float yMax = -2.0f;
+
+    // Use this for initialization
+    void Start () {
         
+        // Spawn each enemy
         foreach( Transform child in transform)
         {
             // Quaternion is rotation
@@ -16,11 +30,47 @@ public class EnemySpawner : MonoBehaviour {
             // Set Enemy Spawner as the parent
             enemy.transform.parent = child;
         }
+
+        // Distance between formation and camera
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));    // Left Corner
+        Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, distance));   // Right Corner
+        xMin = leftBoundary.x + xPadding;
+        xMax = rightBoundary.x - xPadding;
+        yMin = leftBoundary.y + yPadding;
+        yMax = rightBoundary.y - yPadding;
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width,height,0));
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Formation moving right
+        if (isMovingRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+        float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+
+        if (leftEdgeOfFormation < xMin || rightEdgeOfFormation > xMax)
+        {
+            isMovingRight = !isMovingRight;
+            transform.position += Vector3.down * flySpeed * Time.deltaTime;
+        }
         
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Restrict the formation to the game space
+        //float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        float newY = Mathf.Clamp(transform.position.y, yMin, yMax);
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
 }
