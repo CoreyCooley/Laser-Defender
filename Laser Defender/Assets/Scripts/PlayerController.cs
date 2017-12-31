3942f4558ca3f31e9290117c062d2e72b12b0060
Corey Cooley <corey.a.cooley@gmail.com>
2017-12-30 21:22:18 -0500

    Add PlayerController
    Add simple player controller to move left, right, up, and down.

---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 15.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move Player
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0,0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {                        
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);            
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);            
+        }
+    }
+}
